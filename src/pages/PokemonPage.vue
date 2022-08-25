a07247905d7a97e1f6489f4b3c73ad1d28ce7796
mediagui <mediagui@gmail.com>
2022-08-25 10:25:23 +0200

    Diseño de los componentes

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <h1>¿Quén es este pokémon?</h1>
+
+    <PokemonPicture />
+    <PokemonOptions />
+
+</template>
+
+<script>
+
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
+
+
+
+export default {
+  components: { 
+    PokemonPicture,
+    PokemonOptions
+  },
+
+
+}
+</script>
+
